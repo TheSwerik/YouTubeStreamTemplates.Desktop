20868d290d122e62aac8e3822bcc11452ac0aaac
TheSwerik <derswerik@gmail.com>
2020-11-19 12:01:39 +0100

    YEEEEEEEEEESSSS

--- YouTubeStreamTemplates/src/LiveStream/LiveStream.cs
+++ YouTubeStreamTemplates/src/LiveStream/LiveStream.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using Google.Apis.YouTube.v3.Data;
 
 namespace YouTubeStreamTemplates.LiveStream
 {
-    //TODO renmae
-    public struct LiveStream : IComparable<LiveStream>
+    //TODO rename
+    public class LiveStream : IComparable<LiveStream>
     {
-        public DateTime StartDate { get; init; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public Thumbnail Thumbnail { get; set; }
+        public string[] Playlists { get; set; }
+        public string[] Tags { get; set; }
+        public string Language { get; set; }
+        public string Category { get; set; }
+        public string Game { get; set; }
+        public DateTime StartDate { get; init; }
+        public LiveBroadcast LiveBroadcast { get; init; }
 
         public int CompareTo(LiveStream other) { return StartDate.CompareTo(other.StartDate); }
-
     }
 }
--- YouTubeStreamTemplates/src/Program.cs
+++ YouTubeStreamTemplates/src/Program.cs
@@ -7,18 +7,23 @@ using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
 using YouTubeStreamTemplates.LiveStream;
 
 namespace YouTubeStreamTemplates
 {
     public class Program
     {
+        private YouTubeService _youTubeService;
+
         [STAThread]
         public static void Main()
         {
             try
             {
-                new Program().Run().Wait();
+                var program = new Program();
+                // program.ListStreams().Wait();
+                program.UpdateStream().Wait();
             }
             catch (AggregateException ex)
             {
@@ -26,70 +31,74 @@ namespace YouTubeStreamTemplates
             }
         }
 
-        private async Task Run()
+        private async Task<LiveBroadcast> ListStreams()
         {
-            using var service = await CreateYouTubeService();
-            var request = service.LiveBroadcasts.List("snippet");
+            await SetYouTubeService();
+            var request = _youTubeService.LiveBroadcasts.List("snippet");
             request.Mine = true;
             var response = await request.ExecuteAsync();
 
-            if (response.Items == null) return;
+            if (response.Items == null) return null;
             var streams = new List<LiveStream.LiveStream>();
             foreach (var stream in response.Items)
             {
                 streams.Add(new LiveStream.LiveStream
                             {
+                                Title = stream.Snippet.Title,
+                                Description = stream.Snippet.Description,
+                                Thumbnail = stream.Snippet.Thumbnails.Standard,
                                 StartDate = DateTime.Parse(stream.Snippet.ScheduledStartTime),
-                                Title = stream.Snippet.Title
+                                LiveBroadcast = stream
                             });
                 Console.WriteLine(stream.Snippet.ScheduledStartTime + " - " + stream.Snippet.Title);
             }
 
             Console.WriteLine();
-            streams.Sort(LiveStreamComparer.ByDateAsc);
-            foreach (var stream in streams) Console.WriteLine(stream.StartDate + "\t" + stream.Title);
-
-            Console.WriteLine();
             streams.Sort(LiveStreamComparer.ByDateDesc);
-            foreach (var stream in streams) Console.WriteLine(stream.StartDate + "\t" + stream.Title);
-
-            Console.WriteLine();
-            streams.Sort(LiveStreamComparer.ByTitleDesc);
-            foreach (var stream in streams) Console.WriteLine(stream.StartDate + "\t" + stream.Title);
-
-            Console.WriteLine();
-            streams.Sort(LiveStreamComparer.ByTitleAsc);
-            foreach (var stream in streams) Console.WriteLine(stream.StartDate + "\t" + stream.Title);
+            foreach (var stream in streams)
+                Console.WriteLine(stream.StartDate + "\t" + stream.Title + "\t" + stream.Thumbnail?.Url);
+            return streams[0].LiveBroadcast;
         }
 
-        private async Task<UserCredential> GetCredentials()
+        private async Task UpdateStream()
+        {
+            var liveBroadcast = await ListStreams();
+            liveBroadcast.Snippet.Title = "test";
+            await SetYouTubeService();
+            var request = _youTubeService.LiveBroadcasts.Update(liveBroadcast, "snippet");
+            var response = await request.ExecuteAsync();
+
+            if (response == null) return;
+            Console.WriteLine(response.Snippet.ScheduledStartTime + " - " + response.Snippet.Title);
+        }
+
+        private async Task<UserCredential> GetCredentials(IEnumerable<string> scopes)
         {
             await using var stream = new FileStream(@"..\..\..\..\client_id.json", FileMode.Open, FileAccess.Read);
             return await GoogleWebAuthorizationBroker.AuthorizeAsync(
                        GoogleClientSecrets.Load(stream).Secrets,
-                       new[]
-                       {
-                           // YouTubeService.Scope.Youtube,
-                           // YouTubeService.Scope.Youtubepartner,
-                           YouTubeService.Scope.YoutubeReadonly
-                           // YouTubeService.Scope.YoutubeUpload,
-                           // YouTubeService.Scope.YoutubeForceSsl,
-                           // YouTubeService.Scope.YoutubepartnerChannelAudit,
-                           // YouTubeService.Scope.YoutubeChannelMembershipsCreator,
-                       },
-                       "Swerik", //TODO why does my username have to be here
+                       scopes,
+                       "user",
                        CancellationToken.None,
-                       new FileDataStore("YouTube.ListMyVideos"));
+                       new FileDataStore("YouTube.Test2"));
         }
 
-        private async Task<YouTubeService> CreateYouTubeService()
+        private async Task<YouTubeService> CreateYouTubeService(params string[] scopes)
         {
             return new(new BaseClientService.Initializer
                        {
-                           HttpClientInitializer = await GetCredentials(),
+                           HttpClientInitializer = await GetCredentials(scopes),
                            ApplicationName = "YouTube Sample",
                            ApiKey = await File.ReadAllTextAsync(@"..\..\..\..\apikey.txt")
                        });
+        }
+
+        ~Program() { _youTubeService?.Dispose(); }
+
+        private async Task SetYouTubeService()
+        {
+            _youTubeService ??= await CreateYouTubeService(YouTubeService.Scope.YoutubeReadonly,
+                                                           YouTubeService.Scope.YoutubeForceSsl);
         }
     }
 }
