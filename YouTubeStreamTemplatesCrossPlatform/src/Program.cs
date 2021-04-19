2f8a81a98e754432f1df54f293b2eae6f6d2cf9c
Swerik <derswerik@gmail.com>
2021-04-19 15:35:32 +0200

    asljkdsa

--- YouTubeStreamTemplatesCrossPlatform/src/Program.cs
+++ YouTubeStreamTemplatesCrossPlatform/src/Program.cs
@@ -6,7 +6,7 @@ namespace YouTubeStreamTemplatesCrossPlatform
     {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
+        // yet and stuff might break. 
         public static void Main(string[] args)
         {
             BuildAvaloniaApp()
