e1bcd713cf0ccdc1beac7cf648e693438b908191
Swerik <derswerik@gmail.com>
2021-06-09 16:14:37 +0200

    Login

--- YouTubeStreamTemplates.UI/ViewModels/LoginViewModel.cs
+++ YouTubeStreamTemplates.UI/ViewModels/LoginViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace YouTubeStreamTemplates.UI.ViewModels
+﻿using System;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace YouTubeStreamTemplates.UI.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
+        public LoginViewModel()
+        {
+            LoginCommand = ReactiveCommand.CreateFromTask(async () => { Console.WriteLine("LOGIN"); });
+        }
+
+        public ICommand LoginCommand { get; }
     }
 }
