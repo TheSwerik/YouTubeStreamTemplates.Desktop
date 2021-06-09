01f0197d5043f5beb6862a51ba0463cb13ca7a41
Swerik <derswerik@gmail.com>
2021-06-09 14:57:02 +0200

    add MVVM project

---
+++ YouTubeStreamTemplates.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace YouTubeStreamTemplates.UI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ YouTubeStreamTemplates.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace YouTubeStreamTemplates.UI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
