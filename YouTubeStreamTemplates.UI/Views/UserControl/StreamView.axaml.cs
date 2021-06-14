f0aea101af5be7d8f3592d7a66f6d599c49c0009
Swerik <derswerik@gmail.com>
2021-06-14 15:42:54 +0200

    add Home View

---
+++ YouTubeStreamTemplates.UI/Views/UserControl/StreamView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Markup.Xaml;
+
+namespace YouTubeStreamTemplates.UI.Views.UserControl
+{
+    public class StreamView : Avalonia.Controls.UserControl
+    {
+        public StreamView() { InitializeComponent(); }
+
+        private void InitializeComponent() { AvaloniaXamlLoader.Load(this); }
+    }
+}
