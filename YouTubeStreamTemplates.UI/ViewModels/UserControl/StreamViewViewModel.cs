7b71b856dc89e1410499b7ac0e6e0d14b3cdb1a3
Swerik <derswerik@gmail.com>
2021-06-29 16:29:44 +0200

    hm

---
+++ YouTubeStreamTemplates.UI/ViewModels/UserControl/StreamViewViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using YouTubeStreamTemplates.LiveStreaming;
+
+namespace YouTubeStreamTemplates.UI.ViewModels.UserControl
+{
+    public class StreamViewViewModel : ViewModelBase
+    {
+        public StreamViewViewModel() { Console.WriteLine("VIEW MODEL"); }
+
+        public string Categories => "Test";
+
+        public void OpenStream()
+        {
+            if (!LiveStreamService.IsInitialized || LiveStreamService.Instance.CurrentLiveStream == null) return;
+            Process.Start(new ProcessStartInfo
+                          {
+                              UseShellExecute = true,
+                              FileName = "https://www.youtube.com/watch?v=" +
+                                         LiveStreamService.Instance.CurrentLiveStream.Id
+                          })
+                   ?.Dispose();
+        }
+    }
+}
--- YouTubeStreamTemplates.UI/Views/UserControl/StreamView.axaml.cs
+++ YouTubeStreamTemplates.UI/Views/UserControl/StreamView.axaml.cs
@@ -1,11 +1,19 @@
-﻿using Avalonia.Markup.Xaml;
+﻿using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using YouTubeStreamTemplates.UI.ViewModels.UserControl;
 
 namespace YouTubeStreamTemplates.UI.Views.UserControl
 {
-    public class StreamView : Avalonia.Controls.UserControl
+    public class StreamView : ReactiveUserControl<StreamViewViewModel>
     {
         public StreamView() { InitializeComponent(); }
 
         private void InitializeComponent() { AvaloniaXamlLoader.Load(this); }
+
+        private void ThumbnailImage_OnClick(object? sender, PointerReleasedEventArgs pointerReleasedEventArgs)
+        {
+            ViewModel.OpenStream();
+        }
     }
 }
